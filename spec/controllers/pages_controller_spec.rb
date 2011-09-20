c4a5456304b3f362c82f16e7da2a6a77ae719e28
Camilo Arthur <camilo.arthur@gmail.com>
2011-09-19 22:46:51 -0300

    Added layout links with routes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -39,15 +39,26 @@ describe PagesController do
   end
   
     describe "GET 'about'" do
-    it "should be successful" do
-      get 'about'
-      response.should be_success
-    end
+      it "should be successful" do
+        get 'about'
+        response.should be_success
+      end
     
-    it "should have the right title" do
-      get 'about'
-      response.should have_selector("title",:content => "#{@base_title} | About")
+      it "should have the right title" do
+        get 'about'
+        response.should have_selector("title",:content => "#{@base_title} | About") 
+      end
     end
-  end
+      
+    describe "GET 'help'" do  
+      it "should be successful" do
+        get 'help'
+        response.should be_success
+      end
 
+      it "should have the right title" do
+        get 'help'
+        response.should have_selector("title",:content => "#{@base_title} | Help")
+      end  
+    end
 end
